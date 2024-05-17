5867d9b9c037b482db4637281c2c5abab27a820e
Henry Morgan <76470485+henatic@users.noreply.github.com>
2024-05-17 15:40:57 -0700

    Turns out some of the library examples of tensorflow are depricated.  Going off course now.

--- CoreLearningAlgorithms.py
+++ CoreLearningAlgorithms.py
@@ -7,3 +7,12 @@ from IPython.display import clear_output
 # from six.moves import urllib
 
 import tensorflow as tf
+
+dataset = tf.data.Dataset.from_tensor_slices((features, labels)).batch(1)
+eval_dataset = tf.data.Dataset.from_tensor_slices(
+      (eval_features, eval_labels)).batch(1)
+
+model = tf.keras.models.Sequential([tf.keras.layers.Dense(1)])
+optimizer = tf.keras.optimizers.Adagrad(learning_rate=0.05)
+
+model.compile(optimizer=optimizer, loss="mse")
